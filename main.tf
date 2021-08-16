1dae88af7612cccd7c5602ad3a6b358fa789ad9e
paragkapoorcs <83000158+paragkapoorcs@users.noreply.github.com>
2021-08-16 15:32:12 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+data "google_client_config" "current" {}
+
+terraform {
+  required_providers {
+    cdap = {
+      source  = "GoogleCloudPlatform/cdap"
+    #  version = "< 1.0"
+    }
+  }
+}
+
+provider "cdap" {
+  host  = "https://example-instance-example-project-dot-usc1.datafusion.googleusercontent.com/api/"
+  token = data.google_client_config.current.access_token
+}
+
+resource "cdap_profile" "test" {
+  name  = "pradeeppoc2"
+  label = "pradeeppoc2"
+  profile_provisioner {
+    name = "gcp-existing-dataproc"
+    properties {
+      name        = "projectId"
+      value       = "dev-cs-1"
+      is_editable = true
+    }
+  }
+}
