cec8f295aec7d9d82ce7ae1095048e107ede8f9d
rajeevsharmaCS <70970731+rajeevsharmaCS@users.noreply.github.com>
2021-08-16 18:15:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,3 @@
-data "google_client_config" "current" {}
-
-terraform {
-  required_providers {
-    cdap = {
-      source  = "GoogleCloudPlatform/cdap"
-    #  version = "< 1.0"
-    }
-  }
-}
-
-provider "cdap" {
-  host  = "https://example-instance-example-project-dot-usc1.datafusion.googleusercontent.com/api/"
-  token = data.google_client_config.current.access_token
-}
-
 module "custom_dataproc" {
   source  = "terraform-google-modules/data-fusion/google//modules/dataproc_profile"
   version = "~> 0.1"
