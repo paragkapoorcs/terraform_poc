da1acbbf93130b36c3be351166954873d89bf410
rajeevsharmaCS <70970731+rajeevsharmaCS@users.noreply.github.com>
2021-08-18 16:43:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,27 @@
 data "google_client_config" "current" {}
 
+terraform {
+  required_providers {
+    cdap = {
+      source  = "GoogleCloudPlatform/cdap"
+    #  version = "< 1.0"
+    }
+  }
+}
+
 provider "cdap" {
   host  = "https://example-instance-example-project-dot-usc1.datafusion.googleusercontent.com/api/"
   token = data.google_client_config.current.access_token
 }
+
 resource "cdap_profile" "profile" {
-    name  = "example-profile"
-    label = "example-profile"
+    name  = "dataproc112"
+    label = "dataproc112"
     profile_provisioner {
         name = "gcp-dataproc"
         properties {
             name        = "projectId"
-            value       = "example-project"
+            value       = "dev-cs-1"
             is_editable = false
         }
     }
