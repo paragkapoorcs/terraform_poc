1cc867508a6c088e1b5677fa1d782cfd7f58a251
rajeevsharmaCS <70970731+rajeevsharmaCS@users.noreply.github.com>
2021-08-20 12:59:38 +0530

    Update main.tf

--- dataproc_profile/main.tf
+++ dataproc_profile/main.tf
@@ -1,28 +1,25 @@
-data "google_client_config" "current" {}
-
-terraform {
-  required_providers {
-    cdap = {
-      source  = "GoogleCloudPlatform/cdap"
-    #  version = "< 1.0"
-    }
-  }
+module "data-fusion_dataproc_profile" {
+  source  = "terraform-google-modules/data-fusion/google//modules/dataproc_profile"
+  version = "0.1.1"
+  # insert the 2 required variables here
 }
+  
+data "google_client_config" "current" {}
 
 provider "cdap" {
   host  = "https://example-instance-example-project-dot-usc1.datafusion.googleusercontent.com/api/"
   token = data.google_client_config.current.access_token
 }
 
-resource "cdap_profile" "test" {
-  name  = "pradeeppoc2"
-  label = "pradeeppoc2"
-  profile_provisioner {
-    name = "gcp-existing-dataproc"
-    properties {
-      name        = "projectId"
-      value       = "dev-cs-1"
-      is_editable = true
-    }
-  }
+module "custom_dataproc" {
+  source  = "terraform-google-modules/data-fusion/google//modules/dataproc_profile"
+  version = "~> 0.1"
+
+  name   = "custom-dataproc"
+  label  = "Custom Dataproc Profile"
+
+  network         = "example-network"
+  subnet          = "example-subnetwork"
+  service_account = "example-sa"
+  gcs_bucket      = "example-bucket"
 }
